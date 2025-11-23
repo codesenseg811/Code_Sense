fe2205908fc192ff9af96ba804e170a4d2c177a8
shreenithya6 <shreenithyamarrikanti2306@gmail.com>
2025-11-23 13:02:08 +0530

    db.js

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose");
+import dotenv from dotenv
+dotenv.config()
+const connectDB=async()=>{
+    try{
+         await mongoose.connect(process.env.MONGO_URI);
+         console.log("Mongodb connected")
+    }
+    catch(err){
+        console.log("Mongodb failed to connect")
+    }
+}
